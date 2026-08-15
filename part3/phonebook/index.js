177da87cfafef699f6fbc701636bbef63de4b76c
Aditya Dhembare <dhembareaditya4756@gmail.com>
2026-08-15 18:53:43 +0530

    Complete Phonebook backend exercise 3.3

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -38,10 +38,22 @@ app.get('/info', (request, response) => {
   `)
 })
 
+app.get('/api/persons/:id', (request, response) => {
+  const id = request.params.id
+  const person = persons.find(person => person.id === id)
+
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
+})
+
 const PORT = 3001
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`)
   console.log(`Persons API: http://localhost:${PORT}/api/persons`)
   console.log(`Info page: http://localhost:${PORT}/info`)
+  console.log(`Single person: http://localhost:${PORT}/api/persons/1`)
 })
