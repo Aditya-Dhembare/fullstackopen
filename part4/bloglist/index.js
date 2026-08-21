2d461825d3fd5409941d2a24e75a18004d225736
Aditya Dhembare <dhembareaditya4756@gmail.com>
2026-08-21 19:20:48 +0530

    Complete exercise 4.2 Blog List refactoring

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -1,45 +1,7 @@
-const express = require('express')
-const mongoose = require('mongoose')
-require('dotenv').config()
+const app = require('./app')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
 
-const app = express()
-
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
+app.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 })
-
-const Blog = mongoose.model('Blog', blogSchema)
-
-app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs)
-  })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog.save().then((result) => {
-    response.status(201).json(result)
-  })
-})
-
-const PORT = process.env.PORT || 3003
-
-mongoose
-  .connect(process.env.MONGODB_URI)
-  .then(() => {
-    console.log('connected to MongoDB')
-
-    app.listen(PORT, () => {
-      console.log(`Server running on port ${PORT}`)
-    })
-  })
-  .catch((error) => {
-    console.log('error connecting to MongoDB:', error.message)
-  })
